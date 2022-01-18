78975485d81d7bad411721bd6492c3c1bdf897a6
moophlo <andrea.odorisio@gmail.com>
2022-01-18 13:02:55 +0100

    Update clear_resync.sh

--- clear_resync.sh
+++ clear_resync.sh
@@ -17,6 +17,7 @@ echo "Starting the miner ..."
 sudo docker start miner
 echo "Downloading last available blockchain snapshot ..."
 sudo wget https://helium-snapshots.nebra.com/snap-$SNAPSHOT -O /home/pi/hnt/miner/snap/snap-$SNAPSHOT
+sleep 10
 echo "Importing snapshot into the miner - this will take aproximately 20 minutes"
 sudo sudo docker exec miner miner snapshot load /var/data/snap/snap-$SNAPSHOT
 echo "You can safely ignore the timeout error!"
