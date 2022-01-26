65aa08c089eb1f21e06f497a8da5d4bcebe5fa68
moophlo <andrea.odorisio@gmail.com>
2022-01-26 16:34:53 +0100

    Update clear_resync.sh

--- clear_resync.sh
+++ clear_resync.sh
@@ -4,20 +4,13 @@ SNAPSHOT=`curl --silent https://helium-snapshots.nebra.com/latest.json|awk '{pri
 echo "Stopping the miner ..."
 sudo docker stop miner
 echo "Clearing blockchain data ..."
-sudo rm -rf /home/pi/hnt/miner/blockchain.db/00*
-sudo rm -rf /home/pi/hnt/miner/blockchain.db/01*
-sudo rm -rf /home/pi/hnt/miner/blockchain.db/02*
-sudo rm -rf /home/pi/hnt/miner/blockchain.db/03*
-sudo rm -rf /home/pi/hnt/miner/blockchain.db/04*
-sudo rm -rf /home/pi/hnt/miner/blockchain.db/05*
-sudo rm -rf /home/pi/hnt/miner/blockchain.db/06*
-sudo rm -rf /home/pi/hnt/miner/blockchain.db/*
-sudo rm -rf /home/pi/hnt/miner/ledger.db/*
+sudo rm -rf /home/pi/hnt/miner/blockchain.db
+sudo rm -rf /home/pi/hnt/miner/ledger.db
 echo "Starting the miner ..."
 sudo docker start miner
 echo "Downloading last available blockchain snapshot ..."
 sudo wget https://helium-snapshots.nebra.com/snap-$SNAPSHOT -O /home/pi/hnt/miner/snap/snap-$SNAPSHOT
-sleep 10
+sleep 20
 echo "Importing snapshot into the miner - this will take aproximately 20 minutes"
 sudo sudo docker exec miner miner snapshot load /var/data/snap/snap-$SNAPSHOT
 echo "You can safely ignore the timeout error!"
