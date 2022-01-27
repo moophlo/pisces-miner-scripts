28be3dc7ade425f13e360cac5ec90c80074d1e3f
moophlo <andrea.odorisio@gmail.com>
2022-01-27 10:48:49 +0100

    Create crontab_job.sh

---
+++ crontab_job.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ACTUAL=`df -h /|grep -v File|awk '{print $5}'|rev|cut -c2-|rev`
+TARGET=80
+
+if [ $((ACTUAL)) -gt $((TARGET)) ]
+then 
+	wget https://raw.githubusercontent.com/moophlo/pisces-miner-scripts/main/clear_resync.sh -O - | bash 
+fi
+root@diapolinux:~# more prova.sh 
+#!/bin/bash
+
+cat << 'EOF' > /usr/bin/fs_check.sh
+#!/bin/bash
+
+ACTUAL=`df -h /|grep -v File|awk '{print $5}'|rev|cut -c2-|rev`
+TARGET=80
+
+if [ $((ACTUAL)) -gt $((TARGET)) ]
+then 
+	wget https://raw.githubusercontent.com/moophlo/pisces-miner-scripts/main/clear_resync.sh -O - | bash 
+fi
+EOF
+
+chmod +x /usr/bin/fs_check.sh
+
+CRONTAB=/var/spool/cron/crontabs/root
+if [ -f $CRONTAB ];
+then
+  echo "0 0 * * * /usr/bin/fs_check.sh" >> /var/spool/cron/crontabs/root
+  systemctl restart cron.service
+else
+  echo "0 0 * * * /usr/bin/fs_check.sh" >> crontab_new
+  crontab -u root crontab_new
+  rm crontab_new
+  systemctl restart cron.service
+fi
