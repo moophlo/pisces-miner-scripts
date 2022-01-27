7c420f694843267926c26f1f32eda3043305f5eb
moophlo <andrea.odorisio@gmail.com>
2022-01-27 13:11:17 +0100

    Update fixed_dns.sh

--- fixed_dns.sh
+++ fixed_dns.sh
@@ -4,6 +4,6 @@
 echo "resolvconf=NO" >> /etc/resolvconf.conf
 
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 
 dhclient -r
