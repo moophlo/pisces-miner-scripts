d03fd8f0a88f029d874ec84277a118ea5d7c5527
moophlo <andrea.odorisio@gmail.com>
2022-01-27 11:48:36 +0100

    Update crontab_job.sh

--- crontab_job.sh
+++ crontab_job.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+
+
+cat << 'EOF' > /usr/bin/clear_resync.sh
+#!/bin/bash
+
+SNAPSHOT=`curl --silent https://helium-snapshots.nebra.com/latest.json|awk '{print $2}'| rev | cut -c2- | rev`
+echo "Stopping the miner ..."
+sudo docker stop miner
+echo "Clearing blockchain data ..."
+sudo rm -rf /home/pi/hnt/miner/blockchain.db
+sudo rm -rf /home/pi/hnt/miner/ledger.db
+echo "Starting the miner ..."
+sudo docker start miner
+echo "Downloading last available blockchain snapshot ..."
+sudo wget https://helium-snapshots.nebra.com/snap-$SNAPSHOT -O /home/pi/hnt/miner/snap/snap-$SNAPSHOT
+sleep 20
+echo "Importing snapshot into the miner - this will take aproximately 20 minutes"
+sudo sudo docker exec miner miner snapshot load /var/data/snap/snap-$SNAPSHOT
+echo "You can safely ignore the timeout error!"
+EOF
+
+chmod +x /usr/bin/clear_resync.sh
+
 cat << 'EOF' > /usr/bin/fs_check.sh
 #!/bin/bash
 
@@ -8,7 +31,7 @@ TARGET=80
 
 if [ $((ACTUAL)) -gt $((TARGET)) ]
 then 
-	wget https://raw.githubusercontent.com/moophlo/pisces-miner-scripts/main/clear_resync.sh -O - | bash 
+	/usr/bin/clear_resync.sh
 fi
 EOF
 
